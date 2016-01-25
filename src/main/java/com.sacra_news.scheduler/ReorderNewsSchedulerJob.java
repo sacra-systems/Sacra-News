d40dc113405abad8441d8df429b81088bbbdfa50
R. van Twisk <github@rvt.dds.nl>
2016-01-25 14:25:30 +0100

    Fix an issue that the news item show's as modified whenorderDate is set by the quartz job

--- src/main/java/com.sacra_news.scheduler/ReorderNewsSchedulerJob.java
+++ src/main/java/com.sacra_news.scheduler/ReorderNewsSchedulerJob.java
@@ -1,9 +1,6 @@
 package com.sacra_news.scheduler;
 
-import org.jahia.services.content.JCRCallback;
-import org.jahia.services.content.JCRNodeWrapper;
-import org.jahia.services.content.JCRSessionWrapper;
-import org.jahia.services.content.JCRTemplate;
+import org.jahia.services.content.*;
 import org.jahia.services.scheduler.BackgroundJob;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
@@ -28,48 +25,54 @@ public class ReorderNewsSchedulerJob extends BackgroundJob {
         return new JCRCallback<Object>() {
             @Override
             public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
-                Query q = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [snt:news]", Query.JCR_SQL2);
-                NodeIterator ni = q.execute().getNodes();
-                final Date now = new Date();
-                boolean changed = false;
-                while (ni.hasNext()) {
+                JCRObservationManager.setAllEventListenersDisabled(Boolean.TRUE);
+                try {
+                    Query q = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [snt:news]", Query.JCR_SQL2);
+                    NodeIterator ni = q.execute().getNodes();
+                    final Date now = new Date();
+                    boolean changed = false;
+                    while (ni.hasNext()) {
 
-                    JCRNodeWrapper node = (JCRNodeWrapper) ni.next();
+                        JCRNodeWrapper node = (JCRNodeWrapper) ni.next();
 
-                    final Date republishDate3 = getDate(node, "republishDate3");
-                    final Date republishDate2 = getDate(node, "republishDate2");
-                    final Date republishDate1 = getDate(node, "republishDate1");
+                        final Date republishDate3 = getDate(node, "republishDate3");
+                        final Date republishDate2 = getDate(node, "republishDate2");
+                        final Date republishDate1 = getDate(node, "republishDate1");
 
-                    final Boolean isRepublished3 = getBoolean(node, "isRePublished3");
-                    final Boolean isRepublished2 = getBoolean(node, "isRePublished2");
-                    final Boolean isRepublished1 = getBoolean(node, "isRePublished1");
+                        final Boolean isRepublished3 = getBoolean(node, "isRePublished3");
+                        final Boolean isRepublished2 = getBoolean(node, "isRePublished2");
+                        final Boolean isRepublished1 = getBoolean(node, "isRePublished1");
 
-                    if (republishDate3 != null && now.compareTo(republishDate3) > 0 && (isRepublished3 == null || !isRepublished3)) {
-                        node.setProperty("isRePublished3", true);
-                        final Calendar cal = Calendar.getInstance();
-                        cal.setTime(republishDate3);
-                        node.setProperty("sortingDate", cal);
-                        logger.info("Setting sortingDate to {} on node {} for republishdate 3", republishDate3, node.getPath());
-                        changed = true;
-                    } else if (republishDate2 != null && now.compareTo(republishDate2) > 0 && (isRepublished2 == null || !isRepublished2)) {
-                        node.setProperty("isRePublished2", true);
-                        final Calendar cal = java.util.Calendar.getInstance();
-                        cal.setTime(republishDate2);
-                        node.setProperty("sortingDate", cal);
-                        logger.info("Setting sortingDate to {} on node {} for republishdate 2", republishDate2, node.getPath());
-                        changed = true;
-                    } else if (republishDate1 != null && now.compareTo(republishDate1) > 0 && (isRepublished1 == null || !isRepublished1)) {
-                        node.setProperty("isRePublished1", true);
-                        final Calendar cal = java.util.Calendar.getInstance();
-                        cal.setTime(republishDate1);
-                        node.setProperty("sortingDate", cal);
-                        logger.info("Setting sortingDate to {} on node {} for republishdate 1", republishDate1, node.getPath());
-                        changed = true;
+                        if (republishDate3 != null && now.compareTo(republishDate3) > 0 && (isRepublished3 == null || !isRepublished3)) {
+                            node.getRealNode().setProperty("isRePublished3", true);
+                            final Calendar cal = Calendar.getInstance();
+                            cal.setTime(republishDate3);
+                            node.getRealNode().setProperty("sortingDate", cal);
+                            logger.info("Setting sortingDate to {} on node {} for republishdate 3", republishDate3, node.getPath());
+                            changed = true;
+                        } else if (republishDate2 != null && now.compareTo(republishDate2) > 0 && (isRepublished2 == null || !isRepublished2)) {
+                            node.getRealNode().setProperty("isRePublished2", true);
+                            final Calendar cal = java.util.Calendar.getInstance();
+                            cal.setTime(republishDate2);
+                            node.getRealNode().setProperty("sortingDate", cal);
+                            logger.info("Setting sortingDate to {} on node {} for republishdate 2", republishDate2, node.getPath());
+                            changed = true;
+                        } else if (republishDate1 != null && now.compareTo(republishDate1) > 0 && (isRepublished1 == null || !isRepublished1)) {
+                            node.getRealNode().setProperty("isRePublished1", true);
+                            final Calendar cal = java.util.Calendar.getInstance();
+                            cal.setTime(republishDate1);
+                            node.getRealNode().setProperty("sortingDate", cal);
+                            logger.info("Setting sortingDate to {} on node {} for republishdate 1", republishDate1, node.getPath());
+                            changed = true;
+                        }
                     }
+                    if (changed) {
+                        session.save();
+                    }
+                } finally {
+                    JCRObservationManager.setAllEventListenersDisabled(Boolean.FALSE);
                 }
-                if (changed) {
-                    session.save();
-                }
+
                 return null;
             }
         };
